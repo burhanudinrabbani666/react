a2438c1e21824c6d2421a025e51b3e7673b81a95
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-20 22:39:27 +0700

    add file and edit README.md

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A repository for my journey in learning the JavaScript framework: **React**. I u
 
 Learning material sources:
 
-1. [The Ultimate React Course 2025: React, Next.js, Redux & More]()
-2. [React]()
+1. [The Ultimate React Course 2025: React, Next.js, Redux & More](https://www.udemy.com/course/the-ultimate-react-course/?couponCode=CP251118G4)
+2. [React](https://react.dev/)
 3. [bearmentor]()
+4. [Create React App](https://create-react-app.dev)
+5. [Vite](https://vite.dev/guide)
---
+++ code/script.js
@@ -0,0 +1,37 @@
+`use strict`;
+
+/*
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState("");
+
+  async function getAdvice() {
+    const res = await fetch(`https://api.adviceslip.com/advice`);
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((count) => count++);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return;
+  <p>
+    You Have read <strong> {props.count}</strong> pieces of advices
+  </p>;
+}
+*/
